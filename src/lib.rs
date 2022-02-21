08323cf68aa4c62cae940aad40277a6eee62f658
siva datta bathina <datta.bathina15@gmail.com>
2022-02-21 13:11:49 +0530

    Added tests for Config::new()

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 pub struct Config {
     pub query: String,
@@ -21,4 +21,25 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
     println!("{:#?}", contents);
     Ok(())
+}
+
+#[cfg(test)]
+mod config_tests {
+    use super::*;
+    #[test]
+    fn config_new_should_succeed() {
+        let result_config = Config::new(&[String::from("lib.rs"), String::from("test"), String::from("test.txt")]).unwrap();
+        let expected_config = Config {
+            query: String::from("test"),
+            filename: String::from("test.txt"),
+        };
+        assert_eq!(result_config.query, expected_config.query);
+        assert_eq!(result_config.filename, expected_config.filename);
+    }
+    
+    #[test]
+    #[should_panic]
+    fn config_new_should_fail() {
+        Config::new(&[String::from("test"), String::from("test.txt")]).unwrap();
+    }
 }
