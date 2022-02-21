fd4644723f7bda00b6cb975bb6ba9ff93088f32f
siva datta bathina <datta.bathina15@gmail.com>
2022-02-21 22:20:10 +0530

    Completed search()

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,27 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str>{
+   let mut result = Vec::new();
+   for line in content.lines() {
+       if line.contains(query) {
+           result.push(line)
+       }
+   }
+   result
+}
+
 #[cfg(test)]
 mod config_tests {
     use super::*;
     #[test]
     fn config_new_should_succeed() {
-        let result_config = Config::new(&[String::from("lib.rs"), String::from("test"), String::from("test.txt")]).unwrap();
+        let result_config = Config::new(&[
+            String::from("lib.rs"),
+            String::from("test"),
+            String::from("test.txt"),
+        ])
+        .unwrap();
         let expected_config = Config {
             query: String::from("test"),
             filename: String::from("test.txt"),
@@ -36,10 +51,24 @@ mod config_tests {
         assert_eq!(result_config.query, expected_config.query);
         assert_eq!(result_config.filename, expected_config.filename);
     }
-    
     #[test]
     #[should_panic]
     fn config_new_should_fail() {
         Config::new(&[String::from("test"), String::from("test.txt")]).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
